f445814af06bfb06bce98057dedd6fd490fba732
tamanchichan <tamanchichan@gmail.com>
2025-07-08 11:50:37 -0500

    Added a connection string, and installed 'Microsoft.Data.SqlClient' package

--- WinFormsDAO/Database/ContactsDAO.cs
+++ WinFormsDAO/Database/ContactsDAO.cs
@@ -9,6 +9,7 @@ namespace WinFormsDAO.Database
 {
     public class ContactsDAO
     {
+        string connectionString = "Data Source=localhost\\SQLEXPRESS;Integrated Security=True;Database=Contacts;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=true";
         public List<Contact> Contacts {  get; set; } = new List<Contact>();
     }
 }
