22dc67c36a0f063cff7629cb9db1ca3486d15539
tamanchichan <tamanchichan@gmail.com>
2025-07-08 11:48:16 -0500

    'ContactsDAO' class created

---
+++ WinFormsDAO/Database/ContactsDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsDAO.Class;
+
+namespace WinFormsDAO.Database
+{
+    public class ContactsDAO
+    {
+        public List<Contact> Contacts {  get; set; } = new List<Contact>();
+    }
+}
