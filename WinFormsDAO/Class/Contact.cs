27dd90d0ed793ae20f21d8d4d2c6395af8cac31f
tamanchichan <tamanchichan@gmail.com>
2025-07-08 11:44:33 -0500

    'Contact' class created

---
+++ WinFormsDAO/Class/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsDAO.Class
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public Contact() { }
+
+        public Contact(string firstName, string lastName, string email, string phoneNumber)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
